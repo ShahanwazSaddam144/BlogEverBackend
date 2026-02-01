b194eaafd66e25da92b0b7cd69e5f846a9506c08
wahb-amir <op422010@gmail.com>
2026-02-01 14:46:46 +0500

    sechema index update

--- backend/Database/profile.js
+++ backend/Database/profile.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
 const profileSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  name: { 
+    type: String, 
+    required: true, 
+    index: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
   desc: { type: String, required: true },
   age: { type: Number, required: true },
   role: { type: String, required: true },
-});
+}, { timestamps: true }); 
 
-// Prevent recompiling the model if it already exists
+
 const Profile = mongoose.models.Profile || mongoose.model("Profile", profileSchema);
 
 export default Profile;
